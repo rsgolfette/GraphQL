134e81626d4c2891e528a30f3d4f453453ac32ba
Rodrigo Simões Golfette de Paula <rsgolfette@gmail.com>
2025-06-29 19:28:01 -0300

    Removendo itens desnecessários da index

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Usuario {
-    id: ID
-    nome: String
-    email: String
-  }
+
 
   type Cliente{
     conta : ID
@@ -17,13 +13,13 @@ const typeDefs = gql`
   }
 
   type Query{
-    usuarios: [Usuario]
+
     clientes: [Cliente]
   }
 
 
     type Mutation {
-    criarUsuario(nome: String!, email: String!): Usuario
+
     adicionarCliente(nome: String!, saldo: Float!): Cliente
   }
 
@@ -31,21 +27,12 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    usuarios: () => usuarios,
+
     clientes: () => clientes
   },
 
    Mutation: {
-    criarUsuario: (_, { nome, email }) => {
-      const novoUsuario = {
-        id: usuarios.length + 1,
-        nome,
-        email
-      };
-      usuarios.push(novoUsuario);
-      return novoUsuario;
-    },
-
+    
     adicionarCliente: (_, {nome, saldo}) =>{
       const novoCliente ={
         conta: clientes.length + 1,
@@ -69,12 +56,6 @@ async function startServer() {
     console.log('Servidor rodando em http://localhost:4000/graphql');
   });
 }
-
-const usuarios = [
-  { id: 1, nome: 'Ana', email: 'ana@email.com' },
-  { id: 2, nome: 'Carlos', email: 'carlos@email.com' },
-];
-
 const clientes = [
   { conta: 1, nome: 'Ana', saldo: 105.10 },
   { conta: 2, nome: 'Carlos', saldo: 90.0 },
